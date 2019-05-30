b2a284566eba85e2529996f61534eb9f9c0389f9
arune0127 <arunes.sc@gmail.com>
2019-05-30 18:19:53 +0300

    Add files

---
+++ articleakims.php
@@ -0,0 +1,14 @@
+
+<article>
+
+<?php
+$straipsnis = getArticle(3);
+
+?>
+  <h3> <?php echo $straipsnis['title']; ?> </h3>
+  <p>
+    <?php echo $straipsnis['content']; ?> </h3>
+  </p>
+
+  <h5> <?php echo $straipsnis['content']; ?> </h5>
+</article>

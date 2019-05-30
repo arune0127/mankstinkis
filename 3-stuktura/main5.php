747e5d3b220345f658436a4cba80f0d825faa97a
arune0127 <arunes.sc@gmail.com>
2019-05-30 18:15:37 +0300

    Add files

---
+++ 3-stuktura/main5.php
@@ -0,0 +1,24 @@
+<?php
+
+    echo " main5.php <br />";
+
+    include_once('./db.php');
+
+?>
+
+
+<section>
+
+  <?php
+
+    $i = 0;
+      while ($i < 2) {
+
+      include('./articleakims.php');
+    $i++;
+    }
+
+  ?>
+
+
+</section>
